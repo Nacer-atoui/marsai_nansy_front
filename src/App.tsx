c95d54774eb4115068dcfa34fa27495c11d0cfb6
Nacer-atoui <nacer.atoui@laplateforme.io>
2026-01-27 14:48:12 +0100

    Initialisation frontend React + Vite + TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Frontend React + TypeScript</h1>
+      <p>Prêt à être connecté au Backend !</p>
+    </div>
+  )
+}
+
+export default App
