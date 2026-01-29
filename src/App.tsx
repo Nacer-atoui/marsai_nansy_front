67cefca359cf3c870353ae557f555953abdaa49e
Nacer-atoui <nacer.atoui@laplateforme.io>
2026-01-29 15:54:58 +0100

    chevron nacer

--- src/App.tsx
+++ src/App.tsx
@@ -47,9 +47,9 @@ function App() {
   if (loading) return <p>Chargement des films...</p>;
   if (error) return <p style={{ color: 'red' }}>Erreur : {error}</p>;
 
-  return (
+  return (<>
    <h1>Accueil</h1> 
-  )
+  
     <div className="App" style={{ padding: '20px' }}>
       <h1>Liste des Films</h1>
       
@@ -76,7 +76,7 @@ function App() {
       
       {movies.length === 0 && <p>Aucun film trouvé dans la base de données.</p>}
     </div>
-  );
+</>);
 }
 
 export default App;
