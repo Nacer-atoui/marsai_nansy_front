3d5cdd03ecc96443577674c7c9eff20e40e1eaf2
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2026-02-04 14:22:08 +0100

    salut

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import MarsSection from '../components/MarsSection'
+import FestivalObjectif from '../components/FestivalObjectif'
 
 export default function HomePage() {
   return (
     <>
         <HeroSection />
         <MarsSection />
+        <FestivalObjectif/>
     </>
   )
 }
