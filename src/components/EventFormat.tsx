ccb6aac1f9678ae92505de70fc8d9495cf7f3265
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-02-05 13:39:10 +0100

    début event format

--- src/components/EventFormat.tsx
+++ src/components/EventFormat.tsx
@@ -8,11 +8,7 @@ export default function EventFormat() {
             <H2 texte="Format de la sélection" />
             <p className="text-gray-400 mt-4 max-w-2xl text-lg">Le parcours des oeuvres</p>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div className="relative h-96 rounded-3xl overflow-hidden group shadow-xl border border-slate-800">
-                    
-                </div>
-            </div>
+            <div className=""></div>
         </div>
     </section>
   )
