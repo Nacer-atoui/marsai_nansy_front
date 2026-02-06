cf984f27a1242d1424d00a3eed04d3a19770d085
Nacer-atoui <nacer.atoui@laplateforme.io>
2026-02-06 15:11:14 +0100

    ajout d'une card

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -4,7 +4,7 @@ export default function Location() {
   return (
     <section className="bg-section-light py-20 px-4 font-display ">
       <div className="flex">
-        {/* div coté droit */}
+        {/* div coté gauche */}
         <div className="max-w-7xl mx-30">
           <div className="flex gap-2">
             <MapPin />
@@ -14,8 +14,13 @@ export default function Location() {
             <h1 className="text-mars-orange">LA</h1>
             <h1>PLATEFORME</h1>
           </div>
+            <p className='mt-20 text-xs'>(EX DOCKS DES SUDS)</p>
+            <div className='bg-midnight rounded-2xl mt-20 p-10'>
+                <h2 className='py-2 text-mars-orange text-xl '>SALLE DES SUCRES</h2>
+                <p>Futur sanctuaire des conférences et de la remise des prix de Mars.A.I. Un espace majestueux alliant patrimoine et technologie.</p>
+            </div>
         </div>
-        {/* div coté gauche */}
+        {/* div coté droit */}
         <div className="mx-136">
           <p className="w-50 text-xs">
             4 000 m² d'espaces modulables dans le centre de Marseille, au cœur
