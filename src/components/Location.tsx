92ef8f0b945a25a320a0f2bde0fa3fc66ecf5552
Nacer-atoui <nacer.atoui@laplateforme.io>
2026-02-10 11:50:23 +0100

    mise en page de la map google

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -2,7 +2,7 @@ import { MapPin } from 'lucide-react';
 
 export default function Location() {
   return (
-    <section className="h-screen bg-midnight py-20 px-4 font-display">
+    <section className=" bg-midnight py-20 px-4 font-display">
       {/* 1. max-w-7xl mx-auto : Centre le contenu sur les grands écrans
          2. grid-cols-1 md:grid-cols-2 : 1 colonne sur mobile, 2 colonnes égales sur PC
          3. gap-20 : Crée l'espace entre la gauche et la droite (mieux que des margins)
@@ -57,16 +57,16 @@ export default function Location() {
               restauration. Le point de rencontre de tous les participants.
             </p>
           </div>
-          <div className="w-full h-[450px] rounded-2xl overflow-hidden shadow-lg border border-white/10"></div>
         </div>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.003972515497!2d5.366207075862241!3d43.314176271120196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c13ddc0211b9%3A0xd1642ae4b32c4bc4!2s%C3%89cole%20La%20Plateforme_%20Marseille%20-%20Entr%C3%A9e%20Sud!5e0!3m2!1sfr!2sfr!4v1770648071784!5m2!1sfr!2sfr"
-          className="w-full h-full"
-          style={{ border: 0 }}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Localisation La Plateforme Marseille">
-          </iframe>
+      </div>
+      <div className="w-320 h-[450px] m-auto mt-8 rounded-2xl overflow-hidden shadow-lg border border-white/10">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.003972515497!2d5.366207075862241!3d43.314176271120196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c13ddc0211b9%3A0xd1642ae4b32c4bc4!2s%C3%89cole%20La%20Plateforme_%20Marseille%20-%20Entr%C3%A9e%20Sud!5e0!3m2!1sfr!2sfr!4v1770648071784!5m2!1sfr!2sfr"
+        className="w-full h-full"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Localisation La Plateforme Marseille"
+      ></iframe>
       </div>
     </section>
   );
