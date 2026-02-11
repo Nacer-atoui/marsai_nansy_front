70f385a45fdeaaeb87db82c3d3ed12b3fb9d28df
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-02-11 10:27:59 +0100

    submitPage + SubmitRealisator

---
+++ src/components/SubmitRealisator.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SubmitRealisator() {
+  return (
+    <div>
+      <h3 className="text-white text-2xl font-bold">Soumission du réalisateur</h3>
+    </div>
+  )
+}
