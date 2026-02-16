cf5efc71c1dd30e24601ad3908f6e55e32d13ff7
Nacer-atoui <nacer.atoui@laplateforme.io>
2026-02-16 10:22:26 +0100

    mise à jour bg submitpage

--- src/page/SubmitPage.tsx
+++ src/page/SubmitPage.tsx
@@ -7,7 +7,7 @@ import SubmitMedia from '../components/SubmitMedia'
 
 export default function SubmitPage() {
   return (
-    <section className="bg-[#07091D] py-20 px-4">
+    <section className="bg-midnight py-20 px-4">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-3xl md:text-5xl font-bold text-white uppercase tracking-wider mb-10">Soumission de film</h2>
         <SubmitRealisator />
